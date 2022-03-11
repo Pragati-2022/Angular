555e91dfe00ad8104f75d19891b8bcc7e6eed875
pragati-sanepara <87306009+pragati-sanepara@users.noreply.github.com>
2022-03-11 17:04:05 +0530

    note test

---
+++ test-project/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddNotesComponent } from './notes/add-notes/add-notes.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListNotesComponent } from './notes/add-notes/list-notes/list-notes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddNotesComponent,
+    ListNotesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
